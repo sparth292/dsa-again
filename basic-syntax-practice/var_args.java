420f26ef3e4fd81ab1e36658838f370860bda3f0
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2026-08-10 19:54:16 +0530

    Create var_args.java

---
+++ basic-syntax-practice/var_args.java
@@ -0,0 +1,9 @@
+
+class Main {
+    public static void main(String[] args) {
+        fun("Ayy Parth",12 , 123 , 456 );
+    }
+    static void fun(String s ,int ...v){
+        System.out.println("Heyy Thanks for the input");
+    }
+}
