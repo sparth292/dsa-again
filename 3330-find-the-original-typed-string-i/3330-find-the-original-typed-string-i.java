c4ba33e339deabe1dd09e1cb4b26abb45d442431
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2026-09-17 12:54:01 +0530

    Time: 1 ms (85.14%), Space: 43.4 MB (44.57%) - LeetHub

--- 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
+++ 3330-find-the-original-typed-string-i/3330-find-the-original-typed-string-i.java
@@ -1,6 +1,7 @@
 class Solution {
     public int possibleStringCount(String word) {
         int count = 0;
+        //my dumbass was using hashset b4 
         for(int i = 1 ; i < word.length() ; i++){
             if(word.charAt(i) == word.charAt(i-1)) count++;
         }
