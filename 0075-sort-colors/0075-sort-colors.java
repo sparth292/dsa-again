6e1bbeb5cae9146b88c254dbe73825ffde2956bd
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2026-09-18 14:31:33 +0530

    Time: 0 ms (100%), Space: 43.5 MB (58.12%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int start = 0;
+        int current = 0;
+        int end = nums.length - 1;
+        while(current <= end) {
+            if(nums[current] == 0) {
+                int temp = nums[current];
+                nums[current] = nums[start];
+                nums[start] = temp;
+                start++;
+                current++;
+            }
+            else if(nums[current] == 1) {
+                current++;
+            }
+            else {
+                int temp = nums[current];
+                nums[current] = nums[end];
+                nums[end] = temp;
+                end--;
+            }
+        }
+    }
+}
